b972b75aeae983bfd89e71e5546bb7ed2e969fa0
Jorge Lucas <j.lucas_7@hotmail.com>
2020-05-19 17:29:32 -0300

    atualização da tela Home

--- mobile/src/pages/Home/index.js
+++ mobile/src/pages/Home/index.js
@@ -1,14 +1,60 @@
 import React from 'react';
-import { useRoute } from '@react-navigation/native';
-import { View, Text, TouchableOpacity, Linking } from 'react-native';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import { View, Text, TouchableOpacity, ScrollView, SafeAreaView} from 'react-native';
+import Carousel from '../../components/Carousel/carousel'
+//import Icon from 'react-native-vector-icons/MaterialIcons'
 
 import styles from './styles';
 
 export default function Home() {
+  const route = useRoute();
+  const navigation = useNavigation();
+
+  const ongId = route.params.ong_id;
+
+  const dummyData =
+        [{
+                title: 'Adoção Consciente', url: 'https://cdn.pixabay.com/photo/2015/10/12/14/57/dogs-984015_960_720.jpg',
+                description: "Adote, porque o amor não tem preço nem raça.",
+                id: 1
+
+        },
+        {
+                title: 'Adoção Consciente', url: 'https://cdn.pixabay.com/photo/2015/11/17/13/13/dogue-de-bordeaux-1047521_960_720.jpg',
+                description: "Adotamos um ao outro,  simples  assim: porque eu precisava dele  e ele de mim.",
+                id: 2
+        },
+        {
+                title: 'Adoção Consciente', url: 'https://cdn.pixabay.com/photo/2016/10/31/14/55/rottweiler-1785760_960_720.jpg',
+                description: "Adotar e abrir o coração, faz bem para alma e manda embora a solidão.",
+                id: 3
+        }]
+  
+  function navigateToIncident(ong_id) { 
+    navigation.navigate('Incidents', { ong_id }); 
+  }      
 
   return (
-    <View style={styles.container}>
-      <Text>Home</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+          <Carousel data = {dummyData}/>
+          <View style={styles.cardView}>
+
+            <View style={styles.cardCima}>
+              <TouchableOpacity onPress={() => navigateToIncident(ongId)}>  
+                <Text style={styles.cardTitle}>Ajude</Text>
+              </TouchableOpacity>
+            </View>
+
+            <View style={styles.cardBaixo}>
+              <Text style={styles.cardTitle}>Adote</Text>
+            </View>
+
+          </View>
+          <View style={styles.info}>
+            {/* <Icon name="enviroment" size={20} color="#381564" /> */}
+          </View>
+      </View>
+    </ScrollView>
   );
 }
--- mobile/src/pages/Home/styles.js
+++ mobile/src/pages/Home/styles.js
@@ -1,5 +1,7 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions} from 'react-native';
 import Constants from 'expo-constants';
+
+//const { width, height } = Dimensions.get('window')
 
 export default StyleSheet.create({
   container: {
@@ -8,4 +10,42 @@ export default StyleSheet.create({
     paddingTop: Constants.statusBarHeight + 5,
     backgroundColor: '#F2C94C'
   },
+
+  cardView:{
+    flex: 1,
+/*     width: width,
+    height: height / 3, */
+    //backgroundColor: 'white',
+    margin: 50,
+    borderRadius: 10,
+    flexDirection: "column"
+  },
+
+  cardCima:{
+    flex:1,
+    backgroundColor: '#381564',
+    borderRadius: 10,
+    marginBottom:5
+  },
+
+  cardBaixo:{
+    flex:1,
+    backgroundColor: '#381564',
+    borderRadius: 10,
+    marginTop:5
+  },
+
+  cardTitle:{
+    marginTop:30,
+    color:'white',
+    textAlign:"center",
+    fontSize:40
+  },
+
+   info:{
+/*     flex: 1,
+    //margin: 30,
+    backgroundColor: 'white',
+    flexDirection: "column" */
+  } 
 });
