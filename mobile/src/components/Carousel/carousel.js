057acf70bec79560228bf03c5e32de8f617d17a8
Jorge Lucas <j.lucas_7@hotmail.com>
2020-05-20 15:46:32 -0300

    solução do erro de objeto nulo para scrollToOffset (carousel)

--- mobile/src/components/Carousel/carousel.js
+++ mobile/src/components/Carousel/carousel.js
@@ -20,7 +20,11 @@ function infiniteScroll(dataList){
             scrolled = 0
         }
 
-        this.flatList.scrollToOffset({ animated: true, offset: scrollValue })
+        try{
+            this.flatList.scrollToOffset({ animated: true, offset: scrollValue })
+        }catch{
+            return
+        }
         
     }, 3000)
 }
